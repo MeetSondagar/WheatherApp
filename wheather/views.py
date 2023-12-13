5552eb99ed0a81eae41950aafa52a65329ae29b9
MeetSondagar <meetsondagar030905@gmail.com>
2023-12-13 15:37:55 +0530

    Final Project complete

--- wheather/views.py
+++ wheather/views.py
@@ -7,15 +7,17 @@ import urllib.request
 def index(request):
     if request.method == 'POST':
         city = request.POST['city']
-        res = urllib.request.urlopen('https://api.weatherstack.com/current?access_key=556a2fdc7e96c5b37bda11bd113dc0ff&queary='+city)
+        if " " in city:
+            city.replace(" ", "+")
+        res = urllib.request.urlopen("http://api.weatherstack.com/current?access_key=556a2fdc7e96c5b37bda11bd113dc0ff&query="+city)
         json_data = json.load(res)
         data = {
             'country_code': str(json_data['location']['country']),
-            'coordinates': str(json_data['location']['lon']+ " " + str(json_data['location']['lat'])),
-            'temp': int(json_data['current']['temperature']+'C'),
-            'pressure': int(json_data['current']['pressure']+'C'),
-            'humidity': int(json_data['current']['humidity']+'C')
+            'coordinates': str(json_data['location']['lon'])+ " " + str(json_data['location']['lat']),
+            'temp': int(json_data['current']['temperature']),
+            'pressure': int(json_data['current']['pressure']),
+            'humidity': int(json_data['current']['humidity'])
             }
     else:
         data = {}
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'data': data})
